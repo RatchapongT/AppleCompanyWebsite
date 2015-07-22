01cd7dfbee2a7790de39471cbf20d580b9ec5127
RatchyZanko <ratchapong.t@gmail.com>
2015-07-22 15:59:50 +0700

    Edit user model

--- models/user.js
+++ models/user.js
@@ -3,15 +3,12 @@ var Schema = mongoose.Schema;
 var userService = require('../services/user-service');
 
 var userSchema = new Schema({
-  firstName: {type: String, required: 'Please enter your first name'},
-  lastName: {type: String, required: 'Please enter your last name'},
-  roomNumber: {type: Number, required: 'Please enter your room number', min:[100, 'Not a valid room number']},
-  email: {type: String, required: 'Please enter your email'},
-  password: {type: String, required: 'Please enter your password'},
+  username: String,
+  password: String,
   created: {type: Date, default: Date.now}
 });
 
-userSchema.path('email').validate(function(value, next) {
+userSchema.path('username').validate(function(value, next) {
   userService.findUser(value, function(err, user) {
     if (err) {
       console.log(err);
@@ -19,7 +16,7 @@ userSchema.path('email').validate(function(value, next) {
     }
     next(!user);
   });
-}, 'That email is already in use');
+}, 'That username is already in use');
 
 var User = mongoose.model('User', userSchema);
 
